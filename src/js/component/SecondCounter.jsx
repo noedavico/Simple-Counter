d927df72984e24f3b2bd2d549343942a64300ea2
noedavico <noedavico@hotmail.com>
2023-01-13 23:42:04 +0000

    logica finalizada

---
+++ src/js/component/SecondCounter.jsx
@@ -0,0 +1,51 @@
+
+import React from "react";
+import PropTypes from "prop-types";
+
+
+//Creo el componente del contador
+const SecondCounter = ({ months, weeks, days, hours, minutes, seconds}) => {
+	return (
+	  <>
+		<div className="mainCounter Container-fluid">
+		  <div className="row justify-content-center text-white" >
+			<div className="clock col-2 digit shadow p-2 me-1 rounded-2">
+			  <i className="far fa-clock #offset"></i>
+			</div>
+			<div className="clock col-1 digit shadow p-2 me-1 rounded-2">
+			  {months}
+			</div>
+			<div className="clock col-1 digit shadow p-2 me-1 rounded-3">
+			  {weeks}
+			</div>
+			<div className="clock col-1 digit shadow p-2 me-1 rounded-3">
+			  {days}
+			</div>
+			<div className=" clock col-1 digit shadow p-2 me-1 rounded-3">
+			  {hours}
+			</div>
+			<div className="clock col-1 digit shadow p-2 me-1 rounded-">
+			  {minutes}
+			</div>
+			<div className="clock col-2 digit shadow p-2 rounded-3">
+			  {seconds}
+			</div>
+		  </div>
+		</div>
+	  </>
+	);
+  };
+  
+  // Declaro las props
+  
+  SecondCounter.propTypes = {
+	seconds: PropTypes.number,
+	minutes: PropTypes.number,
+	hours: PropTypes.number,
+	days: PropTypes.number,
+	weeks: PropTypes.number,
+    months: PropTypes.number,
+  };
+  
+  
+  export default SecondCounter;
--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,48 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SecondCounter from "./component/SecondCounter.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let days = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let weeks= 0;
+let months= 0;
+
+
+setInterval(function(){
+  seconds++;
+  if (seconds === 60) {
+    minutes++;
+    seconds = 0;
+  }
+  if (minutes === 60) {
+    hours++;
+    minutes = 0;
+  }
+  if (hours === 24) {
+    days++;
+    hours = 0;
+  }
+  if (days === 7) {
+    weeks++;
+    days = 0;
+  }
+  if (weeks === 4) {
+    months++;
+    weeks = 0;
+  }
+  //render your react application
+  ReactDOM.render(
+    <SecondCounter
+      seconds={seconds}
+      minutes={minutes}
+      hours={hours}
+      days={days}
+      weeks={weeks}
+    months={months}
+    />,
+    document.querySelector("#app")
+  );
+}, 1000);
